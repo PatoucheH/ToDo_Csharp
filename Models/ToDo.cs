deae7a7d2ed18e360c83142f8a2c3ccdba0869a4
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-05-22 13:43:06 +0200

    create repo

---
+++ Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorToDoApp.Models
+{
+   public class ToDo
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; } = false;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public ToDo(string title)
+        {
+            Title = title;
+        }
+    }
+}
+
